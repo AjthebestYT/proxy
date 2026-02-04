8a5f8cdb32f2cf7441216e398b990f8d818cc6bf
AjthebestYT <153687376+AjthebestYT@users.noreply.github.com>
2026-02-04 09:42:55 -0600

    Update UV configuration for new URL paths

--- UV/uv.config.js
+++ UV/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-    prefix: '/active/go/',
-    bare:'https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/',
+    prefix: '/service/', // This is the URL path for proxied sites
+    bare: 'https://bare.benrogu.li/', // This MUST be an active public bare server
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/active/uv/uv.handler.js',
-    bundle: '/active/uv/uv.bundle.js',
-    config: '/active/uv/uv.config.js',
-    sw: '/active/uv/uv.sw.js',
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
 };
